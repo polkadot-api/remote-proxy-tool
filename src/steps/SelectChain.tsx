d117db3bc438a091cf6a756bf99c3f35f2ffc908
Victor Oliva <olivarra1@gmail.com>
2025-06-30 10:42:14 +0200

    select relay + parachain

--- src/steps/SelectChain.tsx
+++ src/steps/SelectChain.tsx
@@ -1,75 +1,128 @@
 import { Input } from "@/components/ui/input";
 import { getHashParam } from "@/lib/hashParams";
 import { smoldot, smoldotChains } from "@/smoldot";
-import { state, useStateObservable } from "@react-rxjs/core";
+import { getRemoteProxySdk } from "@polkadot-api/sdk-remote-proxy";
+import { state, useStateObservable, withDefault } from "@react-rxjs/core";
 import { createSignal } from "@react-rxjs/utils";
 import { Dot } from "lucide-react";
 import { createClient } from "polkadot-api";
 import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
 import { getSmProvider } from "polkadot-api/sm-provider";
-import { getWsProvider } from "polkadot-api/ws-provider/web";
-import { concat, finalize, from, map, NEVER, startWith, switchMap } from "rxjs";
+import { getWsProvider, JsonRpcProvider } from "polkadot-api/ws-provider/web";
+import {
+  combineLatest,
+  concat,
+  finalize,
+  map,
+  NEVER,
+  startWith,
+  switchMap,
+} from "rxjs";
 
 interface SelectedChain {
   type: "sm" | "ws";
-  value: string;
+  relay: string;
+  para: string;
 }
 
 const [selectedChainChange$, setSelectedChain] = createSignal<SelectedChain>();
 
-const initialChainParam = getHashParam("chain");
-export const initialHasChain = !!initialChainParam;
+const initialType = getHashParam("type");
+const initialRelay = getHashParam("relay");
+const initialPara = getHashParam("para");
+export const initialHasChain = !!initialType && !!initialRelay && !!initialPara;
 
-let initialChain: SelectedChain = {
+const initialChain: SelectedChain = {
   type: "sm",
-  value: "polkadot",
+  relay: "kusama",
+  para: "kusamaAh",
 };
-if (initialChainParam) {
-  const [type, ...value] = initialChainParam.split("-");
-  initialChain = {
-    type,
-    value: value.join("-"),
-  } as SelectedChain;
-}
+initialChain.type = (initialType as "sm" | "ws") ?? initialChain.type;
+initialChain.relay = (initialRelay as "sm" | "ws") ?? initialChain.relay;
+initialChain.para = (initialPara as "sm" | "ws") ?? initialChain.para;
 
 export const selectedChain$ = state<SelectedChain>(
   selectedChainChange$,
   initialChain
 );
 
-const getProvider = (selectedChain: SelectedChain) => {
+const getProvider = (
+  selectedChain: SelectedChain
+): {
+  relay: JsonRpcProvider;
+  para: JsonRpcProvider;
+} => {
   if (selectedChain.type === "ws") {
-    return withPolkadotSdkCompat(getWsProvider(selectedChain.value));
+    return {
+      relay: withPolkadotSdkCompat(getWsProvider(selectedChain.relay)),
+      para: withPolkadotSdkCompat(getWsProvider(selectedChain.para)),
+    };
   }
-  return getSmProvider(
-    smoldotChains[selectedChain.value]().then(({ chainSpec }) =>
+
+  const relayChain = smoldotChains[selectedChain.relay]
+    .relay()
+    .then(({ chainSpec }) =>
       smoldot.addChain({
         chainSpec,
       })
-    )
+    );
+  const paraChainSpec = smoldotChains[selectedChain.relay].parachains?.[
+    selectedChain.para
+  ]?.().then((v) => v.chainSpec);
+  const paraChain = Promise.all([relayChain, paraChainSpec]).then(
+    ([relay, chainSpec]) => {
+      if (!chainSpec)
+        throw new Error(
+          `Couldn't load chainSpec ${selectedChain.relay}:${selectedChain.para}`
+        );
+      return smoldot.addChain({
+        chainSpec,
+        potentialRelayChains: [relay],
+      });
+    }
   );
+
+  return {
+    relay: getSmProvider(relayChain),
+    para: getSmProvider(paraChain),
+  };
 };
 
-export const client$ = state(
+export const clients$ = state(
   selectedChain$.pipe(
     switchMap((selectedChain) => {
-      const provider = getProvider(selectedChain);
-      const client = createClient(provider);
+      const { relay, para } = getProvider(selectedChain);
 
-      const client$ = from(client.getUnsafeApi().runtimeToken).pipe(
-        map(() => client),
+      const relayClient = createClient(relay);
+      const paraClient = createClient(para);
+
+      const clients$ = combineLatest([
+        relayClient.getUnsafeApi().runtimeToken,
+        paraClient.getUnsafeApi().runtimeToken,
+      ]).pipe(
+        map(() => ({
+          relayClient,
+          paraClient,
+          sdk: getRemoteProxySdk(relayClient, paraClient),
+        })),
         startWith(null)
       );
-      return concat(client$, NEVER).pipe(
+      return concat(clients$, NEVER).pipe(
         finalize(() =>
           setTimeout(() => {
-            client.destroy();
+            relayClient.destroy();
+            paraClient.destroy();
           })
         )
       );
     })
   ),
   null
+);
+
+export const client$ = clients$.pipeState(
+  map((clients) => clients?.paraClient ?? null),
+  withDefault(null)
 );
 
 export const SelectChain = () => {
@@ -79,43 +132,91 @@ export const SelectChain = () => {
   return (
     <div className="p-2">
       <ul className="flex gap-4 flex-wrap">
-        {Object.keys(smoldotChains).map((chain) => (
-          <li key={chain}>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                checked={selectedChain.value === chain}
-                onChange={() =>
-                  setSelectedChain({
-                    type: "sm",
-                    value: chain,
-                  })
-                }
-              />
-              <div className="py-1">{chain}</div>
-            </label>
-          </li>
-        ))}
+        {Object.keys(smoldotChains)
+          .filter(
+            (chain) =>
+              Object.keys(smoldotChains[chain].parachains ?? {}).length > 0
+          )
+          .map((chain) => (
+            <li key={chain}>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  checked={selectedChain.relay === chain}
+                  onChange={() =>
+                    setSelectedChain({
+                      ...selectedChain,
+                      type: "sm",
+                      relay: chain,
+                      para:
+                        selectedChain.para in smoldotChains[chain].parachains!
+                          ? selectedChain.para
+                          : Object.keys(smoldotChains[chain].parachains!)[0],
+                    })
+                  }
+                />
+                <div className="py-1">{chain}</div>
+              </label>
+            </li>
+          ))}
       </ul>
+      {selectedChain.type === "sm" &&
+      smoldotChains[selectedChain.relay]?.parachains ? (
+        <ul className="flex gap-4 flex-wrap">
+          {Object.keys(smoldotChains[selectedChain.relay].parachains!).map(
+            (chain) => (
+              <li key={chain}>
+                <label className="flex items-center gap-1">
+                  <input
+                    type="radio"
+                    checked={selectedChain.para === chain}
+                    onChange={() =>
+                      setSelectedChain({
+                        ...selectedChain,
+                        type: "sm",
+                        para: chain,
+                      })
+                    }
+                  />
+                  <div className="py-1">{chain}</div>
+                </label>
+              </li>
+            )
+          )}
+        </ul>
+      ) : null}
       <label className="flex items-center gap-2">
         <input
           type="radio"
           checked={selectedChain.type === "ws"}
           onChange={() =>
             setSelectedChain({
+              ...selectedChain,
               type: "ws",
-              value: "",
             })
           }
         />
         <Input
           type="text"
-          placeholder="RPC Endpoint URL"
-          value={selectedChain.type === "ws" ? selectedChain.value : ""}
+          placeholder="RPC Relay URL"
+          value={selectedChain.type === "ws" ? selectedChain.relay : ""}
           onChange={(evt) =>
             setSelectedChain({
+              ...selectedChain,
               type: "ws",
-              value: evt.currentTarget.value,
+              relay: evt.currentTarget.value,
+            })
+          }
+        />
+        <Input
+          type="text"
+          placeholder="RPC Parachain URL"
+          value={selectedChain.type === "ws" ? selectedChain.para : ""}
+          onChange={(evt) =>
+            setSelectedChain({
+              ...selectedChain,
+              type: "ws",
+              para: evt.currentTarget.value,
             })
           }
         />
